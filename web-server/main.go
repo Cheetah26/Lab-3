adc91d0514470b12b1a9b1ab1185516198969039
Cheetah26 <scmongo@gmail.com>
2024-11-26 01:23:07 +0000

    tree

---
+++ web-server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	logger := log.Default()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	addr := "0.0.0.0:" + port
+
+	defaultMessage := os.Getenv("DEFAULT_MESSAGE")
+	if defaultMessage == "" {
+		defaultMessage = "Hello!"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(defaultMessage))
+	})
+
+	// http.HandleFunc("/echo/{message}", func(w http.ResponseWriter, r *http.Request) {
+	// 	response := "Echo: " + r.PathValue("message")
+	// 	w.Write([]byte(response))
+	// })
+
+	logger.Println("Listening on " + addr)
+	logger.Fatal(http.ListenAndServe(addr, nil))
+}
